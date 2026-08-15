ce1048afcb99ce00fa68dbba8aa72f5bd66c73af
Folaxi-Global <viralesv519@gmail.com>
2026-08-15 06:55:32 -0400

    Update astro.config.mjs

--- vartens_hub/astro.config.mjs
+++ vartens_hub/astro.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
+import vercel from '@vercel/astro';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server', // <-- Vital para que funcionen las rutas dinámicas por subdominio
+  adapter: vercel(),
   integrations: [tailwind(), react()],
-  // Importante: Esto asegura que las rutas funcionen bien en Vercel
   trailingSlash: 'never',
   build: {
-    // Vercel espera la salida en la carpeta 'dist'
     outDir: 'dist'
   }
 });
